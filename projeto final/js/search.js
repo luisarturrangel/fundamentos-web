4f5dd269a0f82d01d5dbda3d9b8085819c0ae83e
Luis Artur Rangel <91829643+luisarturrangel@users.noreply.github.com>
2023-11-26 22:29:47 -0300

    não encontrado query

--- projeto final/js/search.js
+++ projeto final/js/search.js
@@ -1,23 +1,38 @@
 $(document).ready(function () {
+    var resultsFound = false;
+  
     $("#myInput").on("keyup", function () {
       var value = $(this).val().toLowerCase();
-      $("#searchResults").empty(); // Limpa os resultados anteriores
-
+      $("#searchResults").empty();
+  
       if (value === "") {
-        $("#main-search .col").show(); // Mostra todas as colunas se a pesquisa estiver vazia
+        $("#main-search .col").show();
+        resultsFound = true;
       } else {
+        resultsFound = false;
         $("#main-search .col").filter(function () {
           var cardText = $(this).text().toLowerCase();
           var cardFound = cardText.indexOf(value) > -1;
-
+  
           if (cardFound) {
             $(this).clone().css('display', '').appendTo("#searchResults");
+            resultsFound = true;
           } else {
             $(this).hide();
           }
-
+  
           return cardFound;
         });
       }
+  
+      // Adiciona a mensagem "Não Encontrado" se nenhum resultado for encontrado
+      var notFoundMessage = $("#not-found");
+      if (!resultsFound && notFoundMessage.is(':empty')) {
+        $("#main-cards .col:hidden").show(); // Mostra todos os cards ocultos
+        $("<h1>").addClass("container text-white text-center").text("Não Encontrado").appendTo("#not-found");
+      } else if (resultsFound) {
+        notFoundMessage.empty(); // Limpa a mensagem "Não Encontrado" se houver resultados
+      }
     });
   });
+  
